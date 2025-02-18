afde6065895952f8317eb17db649084b5809cfda
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2025-02-18 16:50:51 +0530

    Update 99Lines.jl

--- Projects/99Lines.jl
+++ Projects/99Lines.jl
@@ -2,6 +2,16 @@
 
 function song(start::Integer)
     lines(i) = i == 0 ? "No more Lines" : i == 1 ? "1 line" : "$i lines"
+    # also writtena s -
+    # function lines(i)
+    #     if i == 0
+    #         return "No more Lines"
+    #     elseif i == 1
+    #         return "1 line"
+    #     else
+    #         return "$i lines"
+    #     end
+    # end
     for i in start:-1:1
         println("""
                     $(lines(i)) of code on the Screen,
