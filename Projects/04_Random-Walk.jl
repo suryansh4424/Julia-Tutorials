505beb0a3cccbe8e3f5b8326a6f9e1d713cc2d85
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2025-02-22 01:13:55 +0530

    Create 04_Random-Walk.jl

---
+++ Projects/04_Random-Walk.jl
@@ -0,0 +1,29 @@
+#Random walk
+
+function randomwalk(n::Integer)
+    x = [0]
+    y = [0]
+    for i in 1:n
+        step = rand((-1,1))
+        direction = rand(("x","y"))
+        if direction == "x"
+            push!(x, x[i] + step)
+            push!(y, y[i])
+        else
+            push!(x, x[i])
+            push!(y, y[i] + step)
+        end
+    end
+    return x, y
+end
+
+function randomwalkplots(walkers::Integer, steps::Integer)
+    scene = Scene()
+    for i in 1:walkers
+        x,y = randomwalk(steps)
+        lines!(x,y, linewidth = 5, color = (rand(RGBf0),0.5)) #RGB and alpha -> color strenth
+    end
+    scene
+end
+
+
